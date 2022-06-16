21b56e3f9b3ff748577055c5b87262a698a1d38e
Mihir Upadhyay <sirmihirmsd07@gmail.com>
2022-06-16 11:51:58 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import * as React from "react";
+import { Button, View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-export default function App() {
+import Screen1 from "./Screens/screen_1";
+import Screen2 from "./Screens/screen_2";
+import Screen3 from "./Screens/screen_3";
+import Screen4 from "./Screens/screen_4";
+import Screen5 from "./Screens/screen_5";
+
+const Stack = createNativeStackNavigator();
+
+function App() {
     return (
-        <View style={styles.container}>
-            <Text>Hello Peeps!!</Text>
-            <StatusBar style="auto" />
-        </View>
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home">
+                <Stack.Screen name="Screen 1" component={Screen1} />
+                <Stack.Screen name="Screen 2" component={Screen2} />
+                <Stack.Screen name="Screen 3" component={Screen3} />
+                <Stack.Screen name="Screen 4" component={Screen4} />
+                <Stack.Screen name="Screen 5" component={Screen5} />
+            </Stack.Navigator>
+        </NavigationContainer>
     );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-});
+export default App;
